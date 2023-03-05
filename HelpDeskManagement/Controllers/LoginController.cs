a1e7f73ab0c0199b3ab71d4e4de22c76a9499485
bhavyatyagi966@gmail.com <bhavyatyagi966@gmail.com>
2023-03-05 20:34:23 +0530

    latest code 5 march

---
+++ HelpDeskManagement/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Service.Class;
+using Service.Entities;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelpDeskManagement.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        ILogin _login = null;
+        public LoginController()
+        {
+            _login = new Login();
+        }
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            if (ModelState.IsValid == true)
+            {
+                User obj = new User();
+                obj = _login.GetinfoByUserCredentials(user.UserName, user.Password);
+                if (obj == null)
+                {
+                    ViewBag.ErrorMessage = "Login Failed!";
+                    return View();
+                }
+
+
+                else
+                {
+                    Session["username"] = user.UserName;
+                    //Session["password"]=s.password;
+                    return RedirectToAction("Index", "Dashboard");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Service/Class/Login.cs
@@ -0,0 +1,31 @@
+﻿using Model;
+using Service.Entities;
+using Service.Interface;
+using Model;
+using Service.Entities;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Class
+{
+    public class Login : ILogin
+    {
+        HelpDeskDBEntities db1 = new HelpDeskDBEntities();
+        public User GetinfoByUserCredentials(string Username, string Password)
+        {
+            User user = new User();
+            HelpDesk roledata = db1.HelpDesks.Where(m => m.UserName == Username && m.Password == Password).FirstOrDefault();
+            if (roledata != null)
+            {
+                user.UserName = roledata.UserName;
+                user.Password = roledata.Password;
+            }
+            //roletable credentials = roledata.where(Roletable=>)
+            return user;
+        }
+    }
+}
